a6d661c9d21ba95288771a1f28615d0eba70f7c8
Nick Doiron <ndoiron@mapmeld.com>
2023-06-05 22:34:58 -0500

    add alt route due to caching

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,8 +8,9 @@ import * as $1 from "./routes/arxiv-hauls.tsx";
 import * as $2 from "./routes/index.tsx";
 import * as $3 from "./routes/rss/feed.tsx";
 import * as $4 from "./routes/rss/topics/[slug].tsx";
-import * as $5 from "./routes/sitemap.tsx";
-import * as $6 from "./routes/topics/[slug].tsx";
+import * as $5 from "./routes/sitelist.tsx";
+import * as $6 from "./routes/sitemap.tsx";
+import * as $7 from "./routes/topics/[slug].tsx";
 
 const manifest = {
   routes: {
@@ -18,8 +19,9 @@ const manifest = {
     "./routes/index.tsx": $2,
     "./routes/rss/feed.tsx": $3,
     "./routes/rss/topics/[slug].tsx": $4,
-    "./routes/sitemap.tsx": $5,
-    "./routes/topics/[slug].tsx": $6,
+    "./routes/sitelist.tsx": $5,
+    "./routes/sitemap.tsx": $6,
+    "./routes/topics/[slug].tsx": $7,
   },
   islands: {},
   baseUrl: import.meta.url,
--- routes/sitelist.tsx
+++ routes/sitelist.tsx
@@ -4,14 +4,14 @@ import { getPost, getPosts, Post } from "@/utils/posts.ts";
 export const handler: Handlers<Post[]> = {
   async GET(req) {
     const posts = await getPosts();
-    return new Response(SiteMap({ data: posts }),
+    return new Response(SiteList({ data: posts }),
       {
       headers: { "Content-Type": "text/xml" },
     });
   },
 };
 
-export function SiteMap(props: PageProps<Post[]>) {
+export function SiteList(props: PageProps<Post[]>) {
   const posts = props.data;
   return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n` +
     posts.map((post) => `<url>
